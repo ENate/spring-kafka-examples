c8e0a1ed55b7c484ae5400dcf82bdd28752fa2e8
Manes Hi <manes.hipocrates@gmail.com>
2021-05-22 00:53:40 +0200

    Added more content. Added a kafka example in spring boot

---
+++ basic-springboot-kafka/src/main/java/com/minejava/basicspringbootkafka/BasicSpringbootKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.minejava.basicspringbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringbootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringbootKafkaApplication.class, args);
+	}
+}
---
+++ basic-springboot-kafka/src/test/java/com/minejava/basicspringbootkafka/BasicSpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minejava.basicspringbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
