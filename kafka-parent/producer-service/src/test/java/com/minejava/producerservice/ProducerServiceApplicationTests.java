b4589756249fee874cd3ba8d627471808f3f9553
Diogenes4Sinope <talk2nategwu@protonmail.com>
2022-05-29 21:53:56 +0000

    Added child consumer and producer services

---
+++ kafka-parent/producer-service/src/test/java/com/minejava/producerservice/ProducerServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.minejava.producerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerServiceApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerServiceApplication.class, args);
+    }
+}
